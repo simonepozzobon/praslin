63b430f8f94d00ea3646f140751eb800504e030f
simonepozzobon <info@simonepozzobon.com>
2018-05-30 04:30:26 +0200

    Config
    - Setup di laravel mix
    - scaffolding per l'autorizzazione
    - installazione delle varie dipendenze

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,23 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix
+    .js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .extract(['jquery', 'tether', 'bootstrap'])
+    .autoload({
+        jquery: ['$', 'jQuery', 'jquery'],
+        tether: ['Tether'],
+    })
+    .browserSync({
+        proxy: 'http://praslin.test',
+        browser: 'google chrome',
+        port: 3010,
+    })
+    .webpackConfig({
+        resolve:{
+            alias: {
+                'styles': path.resolve(__dirname, 'resources/assets/sass'),
+            }
+        }
+    });
