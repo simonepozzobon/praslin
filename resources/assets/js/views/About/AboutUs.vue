e1201b2fbf3ddae36453271676929cadf828facf
Angus Simons <info@simonepozzobon.com>
2018-06-12 23:09:38 +0200

    Aggiunge il componente delle immagini nella sezione di about

--- resources/assets/js/views/About/AboutUs.vue
+++ resources/assets/js/views/About/AboutUs.vue
@@ -5,7 +5,7 @@
                 <waves class="waves-icon"/>
                 <div class="images">
                     <div id="about-us-img-1">
-                        <img src="/images/about-us-dive-center-2.jpg" class="img-fluid"/>
+                        <image-container src="/images/about-us-dive-center-2.jpg"/>
                     </div>​
                     <div id="about-us-img-2">
                         <img src="/images/equipment-4.jpg" class="img-fluid"/>
@@ -42,6 +42,7 @@
 </template>
 
 <script>
+import ImageContainer from '../../components/SectionTitle.vue'
 import SectionParagraph from '../../components/SectionParagraph.vue'
 import SectionSubtitle from '../../components/SectionSubtitle.vue'
 import SectionTitle from '../../components/SectionTitle.vue'
