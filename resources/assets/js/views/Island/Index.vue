5eb36b58bece36945be17a3a324bc33652bc350d
Angus Simons <info@simonepozzobon.com>
2018-06-13 03:49:18 +0200

    Correzione del testo

--- resources/assets/js/views/Island/Index.vue
+++ resources/assets/js/views/Island/Index.vue
@@ -5,9 +5,6 @@
                 <div class="col-md-6">
                     <waves class="waves-icon"/>
                     <div class="island-images">
-                        <!-- <div id="island-img-1">
-                            <img src="/images/beach-2.jpg" class="img-fluid" />
-                        </div> -->
                         <div id="island-img-2">
                             <img src="/images/praslin-island-1.jpg" class="img-fluid" />
                         </div>
@@ -21,44 +18,45 @@
                     <div class="island-info">
                         <island-description title="Praslin" icon="far fa-map">
                             The second largest island at 38sq km lies 49km (30.5 nautical miles)
-                            north-east of Mahe.<br>
-                            Famed for its COCO DE-MER forest, Praslin is home to one of
-                            Seychelles two UNESCO world heritage site, the VALLE DE MAI.
+                            north-east of Mahe. Famed for its <b>Coco De-Mer</b> forest,
+                            Praslin is home to one of Seychelles two UNESCO world
+                            heritage site, the <b>Valle De Mai</b>.
                         </island-description>
                         <island-description title="Climate" icon="fas fa-thermometer-half">
                             Seychelles has the blissful advantage of a near constant
-                            temperature that rarely drops below 24 degrees Celsius.<br>
-                            Through the year temperature range between 24-32 degrees
-                            Celsius. This warm, tropical climate is marked by two
-                            distinct season:The NORTH-WEST MONSOON and the SOUTH-EAST MONSOON.<br>
-                            The north-west monsoon runs from November to March and the
-                            south-east from June to September both season are named
-                            after the direction of the prevailing wind during these periods.
-                            North-west monsoon brings generally light wind, speed varies
-                            between (15 to 22kph) and highest rainfall is between
-                            December and February.<br>
-                            The South-east monsoon is cooler and drier but the wind is
-                            much stronger, with speeds of up to(60 kph) leading to rough
-                            seas for most of the period.<br>
-                            Best weather usually comes at the change-over between the
-                            monsoons, March/April and October/November, though not
-                            guaranteed, the weather in these months is usually windless
-                            which brings calm seas.<br>
-                            Sea temperature varies from 23-30 degrees celcius.
+                            temperature that rarely drops below 24 degrees Celsius and
+                            during the year <b>temperature range between 24-32 degrees
+                            Celsius</b>.<br>
+
+                            This warm, tropical climate is marked by two distinct season:
+                            the north-west monsoon and the sout-east monsoon.<br>
+                            <br>
+                            <b>North-west monsoon runs from November to March</b> and
+                            brings light wind with speed varying between 15 to 22kph. Highest
+                            rainfall is between December and February.
+                            <b>South-east monsoon runs from June to September</b>,
+                            is cooler and drier but the wind is much stronger with
+                            speeds of up to 60 kph leading to rough seas for most of the period.<br>
+
+                            <b>Best weather</b> usually comes usually from March to April
+                            and from October to November, though not guaranteed,
+                            the weather in these months is usually windless which brings calm seas.<br>
+                            <br>
+                            <b>Sea temperature varies between 23-30 degrees celsius</b>.
                             The changes in monsoons also affect the underwater visibility.
-                            The south-east monsoon brings cold upwellings of plankton,
-                            which can reduce visibility to few metres.
-                            North-West monsoon the visibility is usually better can be
-                            up to 30 metres.
+                            The <b>south-east monsoon</b> brings cold upwellings of plankton,
+                            which can reduce <b>visibility to few metres</b>.
+                            <b>North-West monsoon</b> the visibility is usually better can be
+                            <b>up to 30 metres</b>.
                         </island-description>
                         <island-description title="Languages" icon="far fa-flag">
                             The official languages of Seychelles are Creole, French
                             and English. All three are widely spoken on the Islands,
                             with Creole being the most commonly spoken and English
                             usually used for written communication.<br>
-                            Creole is a french patois.Much of its vocabulary is
+                            Creole is a french patois. Much of its vocabulary is
                             similar to french although somewhat simplified, so it
-                            is not uncommon to hear conversations with a mix of creole,
+                            is not uncommon to hear conversations with mixed creole,
                             french and English words.
                         </island-description>
                     </div>
@@ -69,12 +67,11 @@
                     <section-title title="About The Island" number="04" align="right"/>
                     <div class="island-info">
                         <island-description title="Getting There" icon="far fa-compass">
-                            Seychelles is well served by several Airlines to the
-                            international Airport(SEZ) on Mahe island.<br>
-                            There is a reliable ferry service from Mahe to
-                            Praslin/La Digue with traveling time of a little bit
-                            over an hour.<br>
-                            There are also domestic flights from Mahe to Praslin
+                            Seychelles is well served by several airlines to the
+                            <b>international airport (SEZ) on Mahe island.</b>
+                            From Mahe a reliable ferry service will bring you
+                            to Praslin / La Digue in around an hour.<br>
+                            There are also <b>domestic flights</b> from Mahe to Praslin
                             flying time takes around 15 minutes so it is the quickest
                             way to travel between the two Islands.
                         </island-description>
@@ -84,7 +81,7 @@
                         </island-description>
                         <island-description title="Money" icon="far fa-money-bill-alt">
                             The currency of Seychelles is the Seychelles Rupee(scr)
-                            100 cents make up one Rupee.There are a range of
+                            100 cents make up one Rupee. There is a range of
                             currency denominations in coin and note form, all
                             of which depict beautifully the natural and cultural
                             heritage of seychelles.
