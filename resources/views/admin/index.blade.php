a04128fb3f760cc8e59aa8b76ad029e784eefc57
simonepozzobon <info@simonepozzobon.com>
2018-07-09 18:25:53 +0200

    Add video tutorials

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,4 +1,29 @@
 @extends('layouts.admin.panel')
 @section('content')
-
+<div class="container">
+    <div class="row pb-5">
+        <div class="col pb-5">
+            <h1 class="pb-4">Login and Gallery upload</h1>
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/rgGJuBVExP8?rel=0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+    <div class="row pb-5">
+        <div class="col pb-5">
+            <h1 class="pb-4">Add a new Dive Spot</h1>
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Qdd-PnzVhsU?rel=0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+    <div class="row pb-5">
+        <div class="col pb-5">
+            <h1 class="pb-4">Post on Instagram</h1>
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/4arQpMx8Qgc?rel=0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
